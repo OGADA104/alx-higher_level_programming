568f9603a3cb9e9c10eb2156b8f975e5953df2b3
MICHAEL <michaelogada104@gmail.com>
2023-10-03 18:27:02 +0000

    00...999

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range( 0, 100, 1):
-    print("{0}".format(i), end= '')
-    if i < 99:
-        print(", ", end= '')
-    else:
-        print("")
+for i in range(100):
+    print("{:02}".format(i), end=(", " if i < 99 else "\n"))

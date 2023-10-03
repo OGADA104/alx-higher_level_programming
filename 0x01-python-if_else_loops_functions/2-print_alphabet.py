0110c8cd0c1476c786f6c9cd61fc30221749cf72
MICHAEL <michaelogada104@gmail.com>
2023-10-03 17:28:22 +0000

    print ASCII a to z

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for char_code in range(ord('a'), ord('z') + 1):
-    if chr(char_code) != 'z':
-        print("{}, ".format(chr(char_code)), end='')
-    else:
-        print("{}".format(chr(char_code)), end='')
+    print("{:c}".format(char_code), end='')

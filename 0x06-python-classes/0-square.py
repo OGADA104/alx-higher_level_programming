1bbd0adb26960de421828c3fed4d8cfa661822cc
MICHAEL <michaelogada104@gmail.com>
2023-10-24 18:11:07 +0000

    0. My first square with comments

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This is an empty class that defines a square.
+    The `Square` class serves as a placeholder for square objects.
+    It is currently empty,
+    without any specific attributes or methods.
+
+    Attributes:
+    None
+    Methods:
+    None
+    """
     pass

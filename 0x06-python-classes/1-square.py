5628258a51bfb14932f545f946cf7d71889c7312
MICHAEL <michaelogada104@gmail.com>
2023-10-24 18:38:18 +0000

    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
+
 class Square:
     """
-    This is an empty class that defines a square.
-    The `Square` class serves as a placeholder for square objects.
-    It is currently empty,
-    without any specific attributes or methods.
+    This is a class that defines a square.
 
     Attributes:
         __size (int): The size (side length) of the square.
     """
+
     def __init__(self, size):
         """
         Initializes a square object with a specific size.

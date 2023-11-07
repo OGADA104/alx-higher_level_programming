0899be2de725014003bfa3801cd476d88e22fa86
MICHAEL <michaelogada104@gmail.com>
2023-11-07 17:31:13 +0000

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" my readfile module"""
+
+
+def read_file(filename=""):
+    """
+    open my file
+    args: file name
+    """
+    with open(filename, encoding="utf-8") as myfile:
+        myfile.read()

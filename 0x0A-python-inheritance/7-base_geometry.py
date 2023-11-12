345057d5cea8557b5ebfbad1b807b5baa1f557e8
MICHAEL <michaelogada104@gmail.com>
2023-11-12 01:56:24 +0000

    task 7 inheritab=nce

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""basegeometry class"""
+
+
+class BaseGeometry:
+    """my empty baseGeometry class"""
+    pass
+
+    def area(self):
+        """get area of my geometry"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validate integer input"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

d049b42782b881b5dc962b98678eede931906a6e
MICHAEL <michaelogada104@gmail.com>
2023-11-12 18:40:43 +0000

    base class correction

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,19 +3,25 @@
 import unittest
 from models.base import Base
 
+
 class TestBase(unittest.TestCase):
 
     def setUp(self):
+        """setup tests"""
+
         self.b1 = Base()
         self.b2 = Base(99)
         self.b3 = Base()
         self.b4 = Base(-123)
 
     def test_Id(self):
+        """run tests"""
         self.assertEqual(self.b1.id, 1)
         self.assertEqual(self.b2.id, 99)
         self.assertEqual(self.b4.id, -123)
         self.assertEqual(self.b3.id, 2)
 
+
 if __name__ == '__main__':
+    """run test file as main"""
     unittest.main()

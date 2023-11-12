62f032289e90c96524b75f6fb24623ca4d6900f7
MICHAEL <michaelogada104@gmail.com>
2023-11-12 01:27:42 +0000

    task 4 inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ def inherits_from(obj, a_class):
     """ check if obj is instance of class
     inheritance directly or ondirectly
     """
-    return isinstance(obj, a_class) and type(obj) is a_class
+    return isinstance(obj, a_class)

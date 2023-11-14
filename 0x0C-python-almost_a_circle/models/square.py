3d60fa8fea338435c3b1cea49627c6df98f658e6
MICHAEL <michaelogada104@gmail.com>
2023-11-14 02:51:09 +0000

    dd

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,8 +8,7 @@ class Square(Rectangle):
     """a square is a special rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id)
-        super().__init__(width = size)
-        super().__init__(x)
-        super().__init__(y)
+        super().__init__(size, size, x, y, id)
 
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)

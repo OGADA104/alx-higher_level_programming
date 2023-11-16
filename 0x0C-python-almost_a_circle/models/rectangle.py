3814cb1577448057888a393ed187778daeff78db
MICHAEL <michaelogada104@gmail.com>
2023-11-16 17:34:15 +0000

    confused

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,7 +9,6 @@ from models.base import Base
 
 class Rectangle(Base):
     """class rectangle created"""
-    
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ init method """

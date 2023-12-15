c72002369ec08d3e1a63f2fdce59a68fd011a996
MICHAEL <michaelogada104@gmail.com>
2023-12-15 19:42:43 +0000

    cities in a state

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- list all capital cities of califonia
-SELECT id, name FROM cities WHERE id = (SELECT id from states WHERE id = 1) ORDER BY id;
+SELECT id, name FROM cities WHERE id = (SELECT id FROM states WHERE id = 1) ORDER BY id;

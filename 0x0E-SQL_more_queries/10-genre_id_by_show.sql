4e860a44735e4172cf9d80b2b718e1966fa15b00
MICHAEL <michaelogada104@gmail.com>
2023-12-17 07:33:58 +0000

    import database

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,2 @@
 -- IMPORT DB
-SOURCE hbtn_0d_tvshows.sql;
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.genre_id WHERE tv_shows_genres.genre_id IS NOT NULL;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id WHERE tv_show_genres.genre_id IS NOT NULL ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;

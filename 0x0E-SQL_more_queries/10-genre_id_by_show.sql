3d225d3de76149d4cb108af290eb2422b44f7359
MICHAEL <michaelogada104@gmail.com>
2023-12-17 07:50:23 +0000

    correct ordering

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,7 @@
 -- IMPORT DB
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id WHERE tv_show_genres.genre_id IS NOT NULL ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.genre_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id + 0 ASC;

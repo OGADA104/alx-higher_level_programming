3c14a680db3aec7bc261e9d684cb529c2fc83d3e
MICHAEL <michaelogada104@gmail.com>
2024-01-08 19:24:05 +0000

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const arg = process.argv0.slice(2);
-if (arg) {
+const arg = process.argv[2];
+
+if (!arg) {
   console.log('No argument');
-} else if (args.length === 1) {
-  console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  console.log(arg);
 }

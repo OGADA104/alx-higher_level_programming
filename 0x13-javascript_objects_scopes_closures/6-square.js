bd6707b0efcb87a7abedaf881273734f5bde87e3
MICHAEL <michaelogada104@gmail.com>
2024-01-11 18:48:58 +0000

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += (c !== undefined) ? c : 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;

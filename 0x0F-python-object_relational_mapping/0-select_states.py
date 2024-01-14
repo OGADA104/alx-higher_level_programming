107ec42cb1c5578a27ab7e7da227329062a7b715
MICHAEL <michaelogada104@gmail.com>
2024-01-14 06:35:26 +0000

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-import sys
 import MySQLdb
-""" a script to connect to a db"""
+import sys
+
+def select_states(mysql_username, mysql_password, database_name):
+    try:
+        db = MySQLdb.connect(
+                host='localhost',
+                port=3306,
+                user=mysql_username,
+                passwd=mysql_password,
+                db=database_name)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id")
+        states = cur.fetchall()
+        return states
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+    finally:
+        if 'cur' in locals():
+            cur.close()
+        if 'db' in locals():
+            db.close()
+
 if __name__ == "__main__":
     args = sys.argv
     mysql_username = args[1]
     mysql_password = args[2]
     database_name = args[3]
-
-    db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=database_name)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    states = cur.fetchall()
+    states = select_states(mysql_username, mysql_password, database_name)
     for state in states:
         print(state)
-    cur.close()
-    db.close()

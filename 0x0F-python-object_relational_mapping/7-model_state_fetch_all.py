de10544714d169eb39d9d6222c64312f954c32ad
MICHAEL <michaelogada104@gmail.com>
2024-01-18 20:17:03 +0000

    task7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,12 +4,11 @@
 from sqlalchemy import create_engine, select
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == '__main__':
     db_url = "mysql://root:root@localhost:3306/hbtn_0e_6_usa"
     engine = create_engine(db_url)
-    Base = declarative_base()
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     Session.configure(bind=engine)

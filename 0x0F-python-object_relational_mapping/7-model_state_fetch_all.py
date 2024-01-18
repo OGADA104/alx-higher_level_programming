563326bc3ddf4deeb5c4a5081ec5999101df7186
MICHAEL <michaelogada104@gmail.com>
2024-01-18 20:11:32 +0000

    task7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,17 +2,22 @@
 """List all states"""
 
 from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
-from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 if __name__ == '__main__':
     db_url = "mysql://root:root@localhost:3306/hbtn_0e_6_usa"
     engine = create_engine(db_url)
+    Base = declarative_base()
     Base.metadata.create_all(engine)
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
+    session = Session()
+
     query = select(
             State.id.label('id'),
-            State.name.label('name')).order_by(State.id)
+            State.name.label('name')).order_by(State.id).limit(5)
     result = session.execute(query).fetchall()
     for row in result:
         print(row.id, row.name)

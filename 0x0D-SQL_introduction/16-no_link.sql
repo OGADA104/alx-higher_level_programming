347c0f24aa5a8101e176b37f408d2d6f3408d7bd
MICHAEL <michaelogada104@gmail.com>
2025-11-11 20:13:27 +0300

    introduction to sql-16-no link

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,4 @@
 -- list all records
-SELECT score, name FROM second_table ORDER BY score DESC ,name DESC;
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC ,name DESC;
